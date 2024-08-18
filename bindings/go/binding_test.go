b1a4e2b25148f3cfcb5f241cbc4c49f001a6fb35
Amaan Qureshi <amaanq12@gmail.com>
2024-08-18 18:43:54 -0400

    fix(go): correct test

--- bindings/go/binding_test.go
+++ bindings/go/binding_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 
 	tree_sitter "github.com/smacker/go-tree-sitter"
-	"github.com/tree-sitter/tree-sitter-cpp"
+	tree_sitter_cpp "github.com/tree-sitter/tree-sitter-cpp/bindings/go"
 )
 
 func TestCanLoadGrammar(t *testing.T) {
